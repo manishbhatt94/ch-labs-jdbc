4f8a4cf91e36887286ce318b06074cac41f02b94
Manish Bhatt <manishbhatt594@gmail.com>
2026-04-03 14:03:17 +0530

    Use DriverManager.getConnection to obtain Connection reference object

--- JDBC01/src/com/mainapp/Launch.java
+++ JDBC01/src/com/mainapp/Launch.java
@@ -1,9 +1,39 @@
 package com.mainapp;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Launch {
 
 	public static void main(String[] args) {
 		printJvmInfo();
+		runDemo();
+	}
+
+	private static void runDemo() {
+
+		try {
+			// Load & Register MySQL driver:
+			// (Note: Below line throws ClassNotFoundException checked exception)
+			// Class.forName("com.mysql.cj.jdbc.Driver"); // <- This is optional now.
+			// Manually loading the Driver is optional since JDBC Specification version 4.0+
+			// which was supported starting Java SE 6 (in year 2003) onwards.
+
+			String url = "jdbc:mysql://localhost:3306";
+			String username = "root";
+			String password = "manish";
+
+			// (Note: Below line throws SQLException checked exception)
+			Connection connection = DriverManager.getConnection(url, username, password);
+			// <- DriverManager.getConnection is a static factory method which gives
+			// an object of an implementation class of the Connection interface.
+			// Here we see the FACTORY DESIGN PATTERN being used!
+
+			System.out.println(connection); // Prints: com.mysql.cj.jdbc.ConnectionImpl@2f943d71
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	private static void printJvmInfo() {
@@ -16,6 +46,7 @@ public class Launch {
 		System.out.println("Java Version: " + System.getProperty("java.version"));
 		System.out.println("Java Vendor:  " + System.getProperty("java.vendor"));
 		System.out.println("Java Home:    " + System.getProperty("java.home"));
+		System.out.println();
 	}
 
 }
