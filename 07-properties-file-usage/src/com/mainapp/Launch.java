99483cdd74d264b40d77e68db3004ec6c47c84c0
Manish Bhatt <manishbhatt594@gmail.com>
2026-04-29 18:19:53 +0530

    Connect to DB & print connection obj, database name from connection obj

--- 07-properties-file-usage/src/com/mainapp/Launch.java
+++ 07-properties-file-usage/src/com/mainapp/Launch.java
@@ -1,5 +1,9 @@
 package com.mainapp;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Launch {
 
 	public static void main(String[] args) {
@@ -7,6 +11,26 @@ public class Launch {
 		String dbUrl = "jdbc:mysql://localhost:3306/ch_labs_jdbc_01";
 		String dbUser = "root";
 		String dbPassword = "manish";
+
+		Connection connection = null;
+
+		try {
+
+			connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+			System.out.println("Connection object: " + connection);
+			System.out.println("Connection with database: " + connection.getCatalog());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 
 	}
 
