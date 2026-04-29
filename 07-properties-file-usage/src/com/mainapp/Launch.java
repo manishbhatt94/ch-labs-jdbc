c6387a6c041006278f809cd5ba576866f657b025
Manish Bhatt <manishbhatt594@gmail.com>
2026-04-29 18:38:04 +0530

    Loop code & check if runtime code change connects to different database

--- 07-properties-file-usage/src/com/mainapp/Launch.java
+++ 07-properties-file-usage/src/com/mainapp/Launch.java
@@ -8,28 +8,38 @@ public class Launch {
 
 	public static void main(String[] args) {
 
-		String dbUrl = "jdbc:mysql://localhost:3306/ch_labs_jdbc_01";
+		// String dbUrl = "jdbc:mysql://localhost:3306/ch_labs_jdbc_01";
+		String dbUrl = "jdbc:mysql://localhost:3306/jfsseptkart";
 		String dbUser = "root";
 		String dbPassword = "manish";
 
 		Connection connection = null;
 
-		try {
+		while (true) {
+			// =========== LOOP STARTS ===========
 
-			connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
-			System.out.println("Connection object: " + connection);
-			System.out.println("Connection with database: " + connection.getCatalog());
+			try {
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (connection != null) {
-				try {
-					connection.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
+				connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+				System.out.println("Connection object: " + connection);
+				System.out.println("Connection with database: " + connection.getCatalog());
+				System.out.println();
+
+				Thread.sleep(2000);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				if (connection != null) {
+					try {
+						connection.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
 				}
 			}
+
+			// =========== LOOP ENDS ===========
 		}
 
 	}
