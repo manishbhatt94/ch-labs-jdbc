bc2bbf3d5ed7ba61c1c1de7668ce63a76a7dfe45
Manish Bhatt <manishbhatt594@gmail.com>
2026-04-29 17:31:28 +0530

    Scaffolding for (empty) project to demo use of Properties for DB config

---
+++ 07-properties-file-usage/src/com/mainapp/Launch.java
@@ -0,0 +1,13 @@
+package com.mainapp;
+
+public class Launch {
+
+	public static void main(String[] args) {
+
+		String dbUrl = "jdbc:mysql://localhost:3306/ch_labs_jdbc_01";
+		String dbUser = "root";
+		String dbPassword = "manish";
+
+	}
+
+}
