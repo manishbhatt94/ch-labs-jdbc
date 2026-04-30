365f9b30e6ebae3d3d6ec9a2eaaad121562b6c62
Manish Bhatt <manishbhatt594@gmail.com>
2026-04-30 13:03:43 +0530

    Read DB config from properties file
    Note: config file present outside the project directory

--- 07-properties-file-usage/src/com/mainapp/Launch.java
+++ 07-properties-file-usage/src/com/mainapp/Launch.java
@@ -1,24 +1,42 @@
 package com.mainapp;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.util.Properties;
 
 public class Launch {
 
+	/**
+	 * Sample contents of config file:
+	 *
+	 * <pre>
+	 * # mysql.url=jdbc:mysql://localhost:3306/ch_labs_jdbc_01
+	 * mysql.url=jdbc:mysql://localhost:3306/jfsseptkart
+	 * mysql.user=root
+	 * mysql.password=manish
+	 * </pre>
+	 */
+	private static final String CONFIG_FILE_PATH = "C:\\Users\\Manish\\Dev\\dummy-files-dump\\myconfig.properties";
+
 	public static void main(String[] args) {
 
-		// String dbUrl = "jdbc:mysql://localhost:3306/ch_labs_jdbc_01";
-		String dbUrl = "jdbc:mysql://localhost:3306/jfsseptkart";
-		String dbUser = "root";
-		String dbPassword = "manish";
-
 		Connection connection = null;
+		FileInputStream fileInputStream = null;
 
 		while (true) {
 			// =========== LOOP STARTS ===========
 
 			try {
+
+				fileInputStream = new FileInputStream(CONFIG_FILE_PATH);
+
+				Properties properties = new Properties();
+				properties.load(fileInputStream);
+
+				String dbUrl = properties.getProperty("mysql.url");
+				String dbUser = properties.getProperty("mysql.user");
+				String dbPassword = properties.getProperty("mysql.password");
 
 				connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
 				System.out.println("Connection object: " + connection);
@@ -30,12 +48,15 @@ public class Launch {
 			} catch (Exception e) {
 				e.printStackTrace();
 			} finally {
-				if (connection != null) {
-					try {
-						connection.close();
-					} catch (SQLException e) {
-						e.printStackTrace();
+				try {
+					if (fileInputStream != null) {
+						fileInputStream.close();
 					}
+					if (connection != null) {
+						connection.close();
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
 			}
 
